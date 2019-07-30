fe2fcd3a049d063f3f0a1da95bed49bb75e90bd3
Nicolas Morin (nicomo) <nicolas.morin@gmail.com>
2019-07-30 09:28:27 +0200

    vtmany done + license

--- cmd/vtmany.go
+++ cmd/vtmany.go
@@ -15,14 +15,24 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"unicode"
+
+	"github.com/nicomo/oclcapis"
 
 	"github.com/spf13/cobra"
 )
 
+// flag for output filename
+var ofname string
+
 // vtmanyCmd represents the vtmany command
 var vtmanyCmd = &cobra.Command{
-	Use:   "vtmany [input file]",
+	Use:   "vtmany [input file] [OPTIONS]",
 	Short: "Translate many source IDs to VIAF IDs.",
 	Long: `
 	Translate a bunch of Source IDs 
@@ -40,22 +50,75 @@ var vtmanyCmd = &cobra.Command{
 	DNB%7c123456,VIAFID#2
 	etc.
 	`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("vtmany called")
+
+		// open input file
+		f, err := os.Open(args[0])
+		if err != nil {
+			log.Fatalf("could not read input file: %v", err)
+		}
+		defer f.Close()
+
+		// read line by line and add to input slice
+		var input []string
+		fScanner := bufio.NewScanner(f)
+		for fScanner.Scan() {
+			s := fScanner.Text()
+
+			// checks for empty lines and whitespaces
+			if len(s) == 0 {
+				log.Fatalf("file should not have empty lines")
+			}
+			for _, runeValue := range s {
+				if unicode.IsSpace(runeValue) {
+					log.Fatalf("lines should not have spaces, see %s\n", s)
+				}
+			}
+			input = append(input, s)
+		}
+
+		// call WS
+		res, err := oclcapis.ViafTranslateBatch(input)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// prepare result for csv
+		output := [][]string{}
+		for k := range res {
+			output = append(output, []string{k, res[k]})
+		}
+
+		// write result to file
+		file, err := os.Create(ofname + ".csv")
+		if err != nil {
+			log.Fatalf("cannot create file: %v\n", err)
+		}
+		defer file.Close()
+
+		w := csv.NewWriter(file)
+
+		for _, o := range output {
+			if err := w.Write(o); err != nil {
+				log.Fatalln("error writing record to csv:", err)
+			}
+		}
+
+		// Write any buffered data to the underlying writer
+		w.Flush()
+
+		if err := w.Error(); err != nil {
+			log.Fatal(err)
+		}
+
 	},
 }
 
 func init() {
+
+	vtmanyCmd.Flags().StringVarP(&ofname, "output", "o", "vtmany-output", "output filename, without extension, e.g. my-output-file")
+
 	rootCmd.AddCommand(vtmanyCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// vtmanyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// vtmanyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	// vtmanyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
