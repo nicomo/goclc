3cb9fd4d21c6fad6c1efcd97e0eca429ba41ca3b
Nicolas Morin (nicomo) <nicolas.morin@gmail.com>
2019-08-05 09:34:28 +0200

    correct writing of output to file for wci

--- cmd/root.go
+++ cmd/root.go
@@ -61,7 +61,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.PersistentFlags().StringVarP(&ofname, "output", "o", "vtmany-output", "output filename, without extension, e.g. my-output-file")
+	rootCmd.PersistentFlags().StringVarP(&ofname, "output", "o", "output", "output filename, without extension, e.g. my-output-file")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- cmd/wci.go
+++ cmd/wci.go
@@ -20,7 +20,6 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"unicode"
@@ -33,17 +32,17 @@ import (
 // wciCmd represents the wci command
 var wciCmd = &cobra.Command{
 	Use:   "wci [input file] [OPTIONS]",
-	Short: "Retrieve Worldcat Identities infos for a bunch of VIAF IDs",
+	Short: "Retrieve Worldcat Identities infos for a bunch of LCCNs",
 	Long: `Retrieve a Worldcat Identities infos 
-	from Viaf IDs.
+	from LCCNs.
 
-	Input should be a file with 1 viaf ID per line, e.g. 
-	10017193
-	10020421
-	10011312
+	Input should be a file with 1 LCCN per line, prefixed with "lccn-", e.g.
+	lccn-n00000400
+	lccn-n00000522
+	lccn-n00000608
 	etc.
 	
-	Result is a csv with 1 result per line.`,
+	Result is a json file.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("wci called")
@@ -79,14 +78,17 @@ var wciCmd = &cobra.Command{
 			log.Fatalln(err)
 		}
 
-		// json marshal the result
-		resJSON, err := json.MarshalIndent(res, "", "	")
+		// create output file
+		ofile, err := os.Create(ofname + ".json")
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalf("coud not create output file: %v", err)
 		}
+		defer ofile.Close()
 
-		// write result to file
-		err = ioutil.WriteFile(ofname+".json", resJSON, 0644)
+		// json encode result to file
+		encoder := json.NewEncoder(ofile)
+		encoder.SetIndent("", "	")
+		err = encoder.Encode(res)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -95,14 +97,4 @@ var wciCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(wciCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// wciCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// wciCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
